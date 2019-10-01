d847064040f79a62221a9642c9845d5dc3dd7ad6
glevix <glevix.x@gmail.com>
2019-10-01 11:30:19 +0300

    Added smile save functionality

--- driver.py
+++ driver.py
@@ -6,8 +6,10 @@ cascade = cv2.CascadeClassifier(cascade_path)
 
 model = LeNet.load_bagged(5)
 
+
 def process(frame):
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    toSave = image.copy()
     faces = cascade.detectMultiScale(
         image,
         scaleFactor=1.1,
@@ -15,13 +17,20 @@ def process(frame):
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+    count = len(faces)
+    if not count:
+        return frame
+    smiles = 0
     for (x, y, w, h) in faces:
         face = cv2.resize(image[y:y+h, x:x+w], (28, 28))
         is_smile = model.predict_single(face, 0.6)
         if is_smile:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            smiles = smiles + 1
         else:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    if smiles == count:
+        cv2.imwrite('snap.jpg', toSave)
     return frame
 
 
